f7bf62c9299bf3729c74dd8f5002d741c7c56f27
Administrator <Administrator@DESKTOP-244Q4EL>
2021-11-23 13:29:44 +0900

    2021-11-23

---
+++ SpringBoard/src/main/java/com/springbook/view/controller/HandlerMapping.java
@@ -0,0 +1,18 @@
+package com.springbook.view.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HandlerMapping {
+	private Map<String, Controller> mappings;
+
+	public HandlerMapping() {
+		mappings = new HashMap<String, Controller>();
+		mappings.put("/login.do", new LoginController());
+	}
+	
+	public Controller getController(String path) {
+		return mappings.get(path);
+	}
+
+}
